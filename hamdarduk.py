38d632cd285ac2cb212641c96ab8162db2a9c503
HamdardUK <fairdealsintl247@gmail.com>
2024-09-09 05:54:52 +0300

    Create hamdarduk.py
    Hi , Here is a short Script that is showing that Hamdard UK full product list .you can see using this code or visiting https://hamdard.co.uk/

---
+++ hamdarduk.py
@@ -0,0 +1,6 @@
+# Hamdarduk Products.py
+def hamdarduk():
+    print("Carmina Plus, Hamdard Ghutti,Naunehal Herbal Gripe Water,Rooh Afza,Rooh Afza GO,Rooh Afza Light Sugar Free,SAFI,Suduri,Sualin!")
+
+if __name__ == "__main__":
+    hamdarduk()
